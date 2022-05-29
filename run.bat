e393122b6caab54c7a08b3d4ff9a8809adcef5a3
Mateus Hamade <mateus.hamade@aluno.ufop.edu.br>
2022-05-29 15:00:54 -0300

    main

--- run.bat
+++ run.bat
@@ -2,5 +2,4 @@ cd obj && del *.o && cd ..
 gcc -c ./utils/heap.c -o ./obj/heap.o
 gcc -c ./src/interF1.c -o ./obj/interF1.o
 gcc -c ./src/main.c -o ./obj/main.o
-gcc ./obj/main.o ./obj/interF1.o ./obj/heap.o -o jair.exe
-jair.exe
+gcc ./obj/main.o ./obj/interF1.o ./obj/heap.o -o ordena.exe
--- src/main.c
+++ src/main.c
@@ -1,5 +1,39 @@
-#include "interF1.h"
+#include <time.h>
 
-int main() {
-    return teste();
+int main(int argc, char const *argv[]) {
+    printf("TP1 - Estrutura de Dados II\n");
+
+    int metodo = -1;      // metodo de pesquisa a ser executado (1 - 2F fitas, 2 - F+1 fitas, 3 - Quicksort)
+    int quantidade = -1;  // quantidade de registros do arquivo (100, 200, 2k, 20k, 200k, 471.705)
+    int situacao = 1;     // situação de ordem do arquivo (1 - Crescente, 2 - Decrescente, 3 - Desordenado)
+    int opcional = 0;     // parâmetro opcional (1 - imprime os registros, 0 - não imprime os registros)
+
+    if (argv[1] != NULL) {
+        metodo = atoi(argv[1]);
+
+        if (argv[2] != NULL) {
+            quantidade = atoi(argv[2]);
+
+            if (argv[3] != NULL && situacao > 0 && situacao < 4) {
+                situacao = atoi(argv[3]);
+
+                if (argv[5] != NULL) {
+                    opcional = atoi(argv[5]);
+                }
+            }
+        }
+    }
+
+    // Implementar menu de acesso às pesquisas
+    switch (metodo) {
+        case 1:
+            teste();
+            break;
+
+        default:  // opção inválida
+            printf("Opcao invalida!\n");
+            break;
+    }
+
+    return 0;
 }
